ec4671d944004aaf402216fdbeb07755298be45d
Nicksama88 <Nicksama88@gmail.com>
2021-01-28 20:26:59 -0800

    add options for delete and put requests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -13,15 +13,34 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
   } else {
     const blog = new Blog({
-      author: body.author,
-      likes: body.likes || 0,
       title: body.title,
+      author: body.author,
       url: body.url,
+      likes: body.likes || 0,
     })
   
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog.toJSON())
   }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blogInputs = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blogInputs, { new: true })
+  response.json(updatedBlog.toJSON())
 })
 
 module.exports = blogsRouter
