777479cc18dac87ecc740ba1de8d86287fa51bdf
Nicksama88 <Nicksama88@gmail.com>
2021-01-28 17:13:01 -0800

    add api test file and make changes to allow async functions

---
+++ tests/blog_api.test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as JSON', async () => {
+
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
