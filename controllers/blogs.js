5c7653164c33125fa59e9fb24eab9b30dc41a0ce
Nicksama88 <Nicksama88@gmail.com>
2021-01-30 17:01:56 -0800

    add username as attribute of newly added blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,21 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) response.json(blog.toJSON())
+  else response.status(404).end()
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const user = await User.findOne({})
 
   if (body.url === undefined || body.title === undefined) {
     response.status(400).end()
@@ -17,6 +25,7 @@ blogsRouter.post('/', async (request, response) => {
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user._id,
     })
   
     const savedBlog = await blog.save()
