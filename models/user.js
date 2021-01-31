32e9d10decb3edc040892418e0ea4b513e618e3f
Nicksama88 <Nicksama88@gmail.com>
2021-01-30 16:48:56 -0800

    require express-async-errors, require password to be > 3 chars, username required and 3 chars, password required and 3 chars

--- models/user.js
+++ models/user.js
@@ -4,10 +4,15 @@ const uniqueValidator = require('mongoose-unique-validator')
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    required: true,
     unique: true,
+    minlength: 3,
   },
   name: String,
-  passwordHash: String,
+  passwordHash: {
+    type: String,
+    required: true,
+  },
 })
 
 userSchema.plugin(uniqueValidator)
