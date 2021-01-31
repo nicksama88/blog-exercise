c292c5338a4a7f8cfbd56641b4d38cd3af2d1380
Nicksama88 <Nicksama88@gmail.com>
2021-01-30 18:31:26 -0800

    implement toekn-based authentication using jwt

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,8 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { request } = require('express')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -14,9 +16,23 @@ blogsRouter.get('/:id', async (request, response) => {
   else response.status(404).end()
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  } else {
+    return null
+  }
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findOne({})
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (body.url === undefined || body.title === undefined) {
     response.status(400).end()
