859b8e2f87f661c37f0cffc40882e14c48c20b8f
Nicksama88 <Nicksama88@gmail.com>
2021-02-04 13:36:38 -0800

    create production build of frontend

---
+++ build/precache-manifest.7cf4f83e36fce29b1b3931eca6cde2f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e9a58e4a02ae2fd545d9f738265042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793efc16172f42a540a0",
+    "url": "/static/js/2.1ea89e65.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1ea89e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3ab084ce873a578fca",
+    "url": "/static/js/main.5147dba5.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
