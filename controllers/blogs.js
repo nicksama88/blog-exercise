a576511f23229b24e196289a65eddd9652099f1b
Nicksama88 <Nicksama88@gmail.com>
2021-02-03 16:34:33 -0800

    update blog router to give intended user JSON back

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -16,6 +16,7 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+  // validate user token
   const body = request.body
   const token = request.token
   const decodedToken = jwt.verify(token, process.env.SECRET)
@@ -37,12 +38,9 @@ blogsRouter.post('/', async (request, response) => {
   
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
-    const savedUser = await user.save()
-    console.log(savedBlog.user)
-    console.log('saved user is', savedUser)
-    savedBlog.user = {username: savedUser.username, id: savedUser._id.toString()}
-    console.log(savedBlog)
-    response.status(201).json(savedBlog.toJSON())
+    await user.save()
+    const blogAfterUser = await Blog.findById(savedBlog.id).populate('user', { username: 1, name: 1 })
+    response.status(201).json(blogAfterUser.toJSON())
   }
 })
 
@@ -72,7 +70,7 @@ blogsRouter.delete('/:id', async (request, response) => {
       foundBlogList = foundBlogList.slice(0, removeIndex)
         .concat(foundBlogList.slice(removeIndex + 1, blogListLen))
     }
-    await User.findByIdAndUpdate(decodedToken.id, {blogs: foundBlogList}, {new:true})
+    await User.findByIdAndUpdate(decodedToken.id, {blogs: foundBlogList})
     response.status(204).end()
   } else {
     response.status(400).json({ error: 'only user who added the blog may delete' })
